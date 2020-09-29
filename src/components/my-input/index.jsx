30738d70e8f1c1a978bb6c8fd3e108f8180969cb
Nik <n.b.kobzev@gmail.com>
2020-09-29 12:31:06 +0300

    error correction

--- src/components/my-input/index.jsx
+++ src/components/my-input/index.jsx
@@ -1,26 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
 
-export default class Myinpunts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.inputNumberValidate = this.inputNumberValidate.bind(this);
-      }
-    
-    inputNumberValidate(event) {
-        const regexp = /[0-9]/g;
-        const validCharsArray = event.target.value.match(regexp) || [];
-        this.setState({value: validCharsArray.join("")});
-      }
+export default function Myinpunts(props) {
 
-    render() {
-        return (
-            <div className = {this.props.divclassName}>
-                <label for={this.props.id} placeholder = {this.props.placeholder} className = {this.props.lableclassName}>{this.props.lable}</label>
-                <input id={this.props.id} type="text"  placeholder={this.props.placeholder} required className = {this.props.inputclassName} value={this.props.value} onChange={this.props.onChange} />
-            </div>
-        );
-    }
+
+    return (
+        <div className={props.divclassName}>
+            <label for={props.id} placeholder={props.placeholder} className={props.lableclassName}>{props.lable}</label>
+            <input id={props.id} type="text" placeholder={props.placeholder} required className={props.inputclassName} value={props.value} onChange={props.onChange} />
+        </div>
+    );
 }
