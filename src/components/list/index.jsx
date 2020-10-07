bfe633a92aed31571ecfabe5478f199e2dc6bc05
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2020-10-07 10:49:46 +0300

    file edited

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import  './index.css';
-import PropTypes from 'prop-types';
+import './index.css';
 
-export default function List(props) {    
-    return (
-        <div >
-          <p>Result sum: </p>
-          <ul className="ul">
-            {props.arraySum.map( (input, index) => <li key={index}>{input}</li>)}
-          </ul>
-        </div>
-    );
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function List(props) {
+  return (
+    <div>
+      <p>Result sum: </p>
+      <ul className="ul">
+        {props.arraySum.map((input, index) => (
+          <li key={index}>{input}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 List.propTypes = {
-  arraySum: PropTypes.arrayOf(PropTypes.number).isRequired
-}
+  arraySum: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
