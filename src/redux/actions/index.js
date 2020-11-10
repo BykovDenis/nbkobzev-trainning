76980f79208d469813850e63c3188360312285b1
Nik <n.b.kobzev@gmail.com>
2020-11-10 16:05:12 +0300

    add widget

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_SUM, INPUT_CHANGE } from '../constants';
+import { ADD_SUM, INPUT_CHANGE, ON_DATA_TABLE_SORT, SET_WIDGET_DATA } from '../constants';
 
 export const inputChange = (name, value) => {
   return {
@@ -14,3 +14,42 @@ export const addSumAC = (arraySum, inputsValue1, inputsValue2, inputsValue3, cou
     counter: counter + 1,
   },
 });
+
+export const setWidgetData = (
+  icon,
+  city,
+  country,
+  temp,
+  description,
+  speed,
+  pressure,
+  humidity,
+  sunrise,
+  sunset,
+  dataTable,
+  dataTime
+) => ({
+  type: SET_WIDGET_DATA,
+  payload: { icon, city, country, temp, description, speed, pressure, humidity, sunrise, sunset, dataTable, dataTime },
+});
+
+export const onSort = (id, dataTable, sort, sortKey) => {
+  let dataTableNew;
+  let sortNew;
+  if (sortKey === id) {
+    if (sort === 'asc') {
+      dataTableNew = [].concat(dataTable).sort((a, b) => (a[id] > b[id] ? -1 : 1));
+      sortNew = 'desc';
+    } else {
+      dataTableNew = [].concat(dataTable).sort((a, b) => (a[id] > b[id] ? 1 : -1));
+      sortNew = 'asc';
+    }
+  } else {
+    dataTableNew = [].concat(dataTable).sort((a, b) => (a[id] > b[id] ? 1 : -1));
+    sortNew = 'asc';
+  }
+  return {
+    type: ON_DATA_TABLE_SORT,
+    payload: { dataTable: dataTableNew, sort: sortNew, sortKey: id },
+  };
+};
